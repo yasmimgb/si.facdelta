44974f71915d8cfffacc94807560880afc4845c3
yasmimgb <63204225+yasmimgb@users.noreply.github.com>
2020-04-06 11:56:24 -0300

    Envio do arquivo
    Código referente a atividade 1 de algoritmos

---
+++ produtosimples.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    
+    public static void main(String args[]) {
+     
+     int numero1 = 8;
+     int numero2 = 6;
+     
+    System.out.println(numero1 * numero2);
+    
+     int PROD =48;
+    
+    }
+}
