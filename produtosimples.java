54311a6e6d237869d55fcacc2f75914772dcf081
yasmimgb <63204225+yasmimgb@users.noreply.github.com>
2020-04-06 12:51:27 -0300

    envio do arquivo

--- produtosimples.java
+++ produtosimples.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
 public class MyClass {
-    
     public static void main(String args[]) {
-     
-     int numero1 = 8;
-     int numero2 = 6;
-     
-    System.out.println(numero1 * numero2);
     
-     int PROD =48;
+    int numero1;
+    int numero2;
+    
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Digite o primeiro número: ");
+    numero1 = sc.nextInt();
+    
+    System.out.println("Digite o segundo número: ");
+    numero2 = sc.nextInt();
+    
+    System.out.println("O produto dos dois números é: " + (numero1 * numero2));
     
     }
 }
